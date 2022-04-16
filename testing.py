0a152c68eb0abf2da3b85041fefe093172368bcf
Nuzhat Zahan <76831249+NuzhatZahan@users.noreply.github.com>
2022-04-16 11:22:02 +0600

    URL -Task done
    Using Selenium accessing the website url

--- testing.py
+++ testing.py
@@ -2,3 +2,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("http://automationpractice.com/index.php")
+print(driver.title)
